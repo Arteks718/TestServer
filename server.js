65ffdf6181a1339172149fe4cfe9d043281bcfef
Arteks718 <artexbro.official@gmail.com>
2022-10-28 12:08:33 +0300

    swagger

--- server.js
+++ server.js
@@ -3,9 +3,35 @@ const express = require("express"),
   port = process.env.port || 3000;
 (routes = require("./routes.js")),
   (mongoose = require("mongoose")),
-  (User = require("./models/UserModels"));
+  (User = require("./models/UserModels")),
+  (swaggerJsDoc = require("swagger-jsdoc")),
+  (swaggerUI = require("swagger-ui-express"));
 
 mongoose.connect("mongodb://localhost/users_db");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "API for users",
+      description: "users",
+      servers: ["http://localhost:3000/"],
+    },
+  },
+  apis: ["./routes.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(express.json());
 routes(app);
